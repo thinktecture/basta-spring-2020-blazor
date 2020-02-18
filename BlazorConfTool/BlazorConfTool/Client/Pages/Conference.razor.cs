19ebbf47629c1a34cf9e1506cc504fe39f5601cb
Christian Weyer <christian.weyer@thinktecture.com>
2020-02-18 08:16:57 +0100

    Preparing for multi mode details

--- BlazorConfTool/BlazorConfTool/Client/Pages/Conference.razor.cs
+++ BlazorConfTool/BlazorConfTool/Client/Pages/Conference.razor.cs
@@ -9,18 +9,36 @@ namespace BlazorConfTool.Client.Pages
     public partial class Conference : ComponentBase
     {
         [Parameter]
+        public string Mode { get; set; }
+
+        [Parameter]
         public Guid Id { get; set; }
+
+        private bool _isShow { get; set; }
 
         [Inject]
         private ConferencesService _conferencesService { get; set; }
 
-        private ConferenceDetails _conference = new ConferenceDetails();
+        private ConferenceDetails _conferenceDetails;
+
+        public Conference()
+        {
+            _conferenceDetails = new ConferenceDetails();
+            _conferenceDetails.DateFrom = DateTime.UtcNow;
+            _conferenceDetails.DateTo = DateTime.UtcNow;
+        }
 
         protected override async Task OnInitializedAsync()
         {
-            var result = await _conferencesService.GetConferenceDetails(Id);
+            _isShow = Mode == Modes.Show;
 
-            _conference = result;
+            switch (Mode)
+            {
+                case Modes.Show:
+                    var result = await _conferencesService.GetConferenceDetails(Id);
+                    _conferenceDetails = result;
+                    break;
+            }
         }
     }
 }
