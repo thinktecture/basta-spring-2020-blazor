68fead43b37241a3f8c47adab38b46bab68fb3e5
Christian Weyer <christian.weyer@thinktecture.com>
2020-02-24 20:34:02 +0100

    More SW stuff

--- BlazorConfTool/BlazorConfTool/Client/pwa/workbox-config.js
+++ BlazorConfTool/BlazorConfTool/Client/pwa/workbox-config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    globDirectory: "bin/Release/netstandard2.1/publish/BlazorConfTool.Client/dist",
+    globDirectory: "../bin/Release/netstandard2.1/publish/BlazorConfTool.Client/dist",
     globPatterns: [
         '**/*.{html,json,js,css,png,ico,json,wasm,dll,eot,otf,woff,svg,ttf}'
     ],
-    swDest: "bin/Release/netstandard2.1/publish/BlazorConfTool.Client/dist/sw.js",
+    swDest: "../bin/Release/netstandard2.1/publish/BlazorConfTool.Client/dist/sw.js",
     navigateFallback: "/index.html",
     clientsClaim: true,
     runtimeCaching: [{
-        urlPattern: "https://api-ttconftool.azurewebsites.net/api/v1/conferences",
+        urlPattern: "https://localhost:44323/api/conferences/",
         handler: "NetworkFirst",
         options: {
             cacheName: "conferencesApi",
