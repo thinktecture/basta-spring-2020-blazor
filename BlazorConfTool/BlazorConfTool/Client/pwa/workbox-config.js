d3c5da1b071b0cc2b59a23320513928a40e97aa5
Christian Weyer <christian.weyer@thinktecture.com>
2020-02-25 09:39:48 +0100

    Add service worker

--- BlazorConfTool/BlazorConfTool/Client/pwa/workbox-config.js
+++ BlazorConfTool/BlazorConfTool/Client/pwa/workbox-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    globDirectory: "../bin/Release/netstandard2.1/publish/BlazorConfTool.Client/dist",
+    globDirectory: "../bin/Debug/netstandard2.1/publish/BlazorConfTool.Client/dist",
     globPatterns: [
-        '**/*.{html,json,js,css,png,ico,json,wasm,dll,eot,otf,woff,svg,ttf}'
+        '**/*.{html,json,js,css,png,ico,json,wasm,dll,pdb,eot,otf,woff,svg,ttf}'
     ],
-    swDest: "../bin/Release/netstandard2.1/publish/BlazorConfTool.Client/dist/sw.js",
+    swDest: "../bin/Debug/netstandard2.1/publish/BlazorConfTool.Client/dist/sw.js",
     navigateFallback: "/index.html",
     clientsClaim: true,
     runtimeCaching: [{
