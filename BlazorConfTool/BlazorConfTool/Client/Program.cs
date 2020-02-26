23a1185f4b00d5ebd3cfb6d25b59f07bb9d456e3
Christian Weyer <christian.weyer@thinktecture.com>
2020-02-26 20:13:01 +0100

    Adding WebWindow-based Desktop app (WIP)

--- BlazorConfTool/BlazorConfTool/Client/Program.cs
+++ BlazorConfTool/BlazorConfTool/Client/Program.cs
@@ -1,14 +1,4 @@
-﻿using BlazorConfTool.Client.Services;
-using Blazored.Toast;
-using Grpc.Net.Client;
-using Grpc.Net.Client.Web;
-using GrpcGreeter;
-using Microsoft.AspNetCore.Blazor.Hosting;
-using Microsoft.AspNetCore.Components;
-using Microsoft.Extensions.DependencyInjection;
-using Sotsera.Blazor.Oidc;
-using System;
-using System.Net.Http;
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
 using System.Threading.Tasks;
 
 namespace BlazorConfTool.Client
@@ -20,32 +10,7 @@ namespace BlazorConfTool.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped<ConferencesService>();
-            builder.Services.AddScoped<CountriesService>();
-
-            builder.Services.AddSingleton(services =>
-            {
-                var httpClient = new HttpClient(
-                    new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
-                var baseUri = services.GetRequiredService<NavigationManager>().BaseUri;
-                var channel = GrpcChannel.ForAddress(
-                    baseUri, new GrpcChannelOptions { HttpClient = httpClient });
-
-                return new Greeter.GreeterClient(channel);
-            });
-
-            builder.Services.AddAlerts();
-
-            builder.Services.AddBlazoredToast();
-
-            builder.Services.AddOptions();
-            builder.Services.AddOidc(new Uri("https://demo.identityserver.io"), (settings, siteUri) =>
-            {
-                settings.UseDefaultCallbackUris(siteUri);
-                settings.ClientId = "spa";
-                settings.ResponseType = "code";
-                settings.Scope = "openid profile email api";
-            });
+            ClientStartup.PopulateServices(builder.Services);
 
             await builder.Build().RunAsync();
         }
